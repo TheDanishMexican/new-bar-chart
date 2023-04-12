7ed742a0eceb1abd38df5612bd34832d1b976fa4
TheDanishMexican <danieljchristjansen@gmail.com>
2023-04-12 11:21:09 +0200

    ændret højden på første søjle

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("js is running");
+
+  const bars = document.querySelectorAll(".bar");
+
+  const firstBar = bars[0];
+
+  const h = 4;
+
+  firstBar.style.height = `${h}vw`;
+}
