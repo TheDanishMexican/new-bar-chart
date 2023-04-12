9bd8aa5e98654c8fa37530959df4cd9a4e75f246
TheDanishMexican <danieljchristjansen@gmail.com>
2023-04-12 11:34:45 +0200

    Tilføjede et array

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 
 window.addEventListener("load", start);
 
+const myArray = [
+  14, 2, 31, 25, 9, 17, 30, 7, 0, 12, 21, 28, 1, 10, 15, 23, 16, 27, 29, 5, 24,
+  19, 6, 8, 20, 3, 32, 13, 26, 18, 4, 11, 22, 0, 31, 9, 17, 12, 21, 16,
+];
+
 function start() {
   console.log("js is running");
 
